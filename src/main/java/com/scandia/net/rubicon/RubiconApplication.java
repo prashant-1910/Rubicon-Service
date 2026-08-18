66c71028629881facf985cfcf6f737356633635d
Prashant <19prashant.bangilwar@gmail.com>
2026-08-18 09:09:49 +0530

    corrected main class

--- src/main/java/com/scandia/net/rubicon/RubiconApplication.java
+++ src/main/java/com/scandia/net/rubicon/RubiconApplication.java
@@ -6,17 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RubiconApplication extends SpringBootServletInitializer {
+public class RubiconApplication {
 
 	//	implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
 	//	runtimeOnly 'org.postgresql:postgresql'
 	//	testImplementation 'org.springframework.boot:spring-boot-starter-data-jpa-test'
 	//providedRuntime 'org.springframework.boot:spring-boot-starter-tomcat-runtime'
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RubiconApplication.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RubiconApplication.class, args);
