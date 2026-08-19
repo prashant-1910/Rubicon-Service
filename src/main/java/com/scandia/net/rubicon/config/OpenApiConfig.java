e195b2e0d567ebd69badea854ef4e464cb23e4aa
Prashant <19prashant.bangilwar@gmail.com>
2026-08-20 00:30:05 +0530

    swagger bean changes

--- src/main/java/com/scandia/net/rubicon/config/OpenApiConfig.java
+++ src/main/java/com/scandia/net/rubicon/config/OpenApiConfig.java
@@ -2,15 +2,23 @@ package com.scandia.net.rubicon.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
 	@Bean
 	public OpenAPI rubiconOpenAPI() {
+		Server server = new Server();
+		server.setUrl("/api");
+		server.setDescription("API Server");
+
 		return new OpenAPI()
+				.servers(List.of(server))
 				.info(new Info()
 						.title("Rubicon API")
 						.description("Rubicon application API documentation")
