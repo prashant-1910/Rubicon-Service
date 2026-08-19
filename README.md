7df5e280e0f4082f99300b1207122e7ce167ff5c
Prashant <19prashant.bangilwar@gmail.com>
2026-08-19 11:49:53 +0530

    Integrated AWS RDS Postgres,added IP in get api to test host

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rubicon-Service
+
+http://15.252.182.241:9090/rubicon/test
+
+http://15.252.182.241:9090/rubicon/swagger-ui/index.html#/student-controller/deleteStudent
--- src/main/java/com/scandia/net/rubicon/controller/TestController.java
+++ src/main/java/com/scandia/net/rubicon/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.scandia.net.rubicon.controller;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-	@GetMapping
-	public String test() {
-		return "Hi There !!!!!";
-	}
+    @GetMapping
+    public String test() {
+        return "Hi There !!!!! Host: " + getHostName() + ", IP: " + getHostIp();
+    }
+
+    private String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            return "unknown";
+        }
+    }
+
+    private String getHostIp() {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (!networkInterface.isUp() || networkInterface.isLoopback()) {
+                    continue;
+                }
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (address instanceof Inet4Address && !address.isLoopbackAddress()) {
+                        return address.getHostAddress();
+                    }
+                }
+            }
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            return "unknown";
+        }
+    }
 }
 //http://52.66.187.237:9090/rubicon/test
 //http://localhost:9090/swagger-ui/index.html#/test-controller/test
